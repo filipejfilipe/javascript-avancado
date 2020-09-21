0896ee3c6721ff9c6ebf2289457da82e8060352b
filipejfilipe <filipe_j_filipe@yahoo.com>
2020-09-22 00:09:24 +0100

    test

--- AppGenerica-0_JavaScript_sem-campos-master/Coisa.js
+++ AppGenerica-0_JavaScript_sem-campos-master/Coisa.js
@@ -1,1 +1,7 @@
-class Coisa {}
+class Coisa {
+    publicField = 'publico';
+    #privateField = 'privado';
+}
+var a = new Coisa;
+console.log(a.publicField); // deu publico
+console.log(a.privateField); // deu undefined
